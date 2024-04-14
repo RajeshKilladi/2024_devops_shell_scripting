74d7329a6fae9ad37c9abb643333503672d18302
Rajesh <rkilladi@gmail.com>
2024-04-15 00:12:10 +0530

    exce3.sh added

---
+++ exce_3.sh
@@ -0,0 +1,9 @@
+#! bin/bash
+# for loop for 1 to 10 numbers
+echo '---------------------------------------------'
+echo ' For Loop Example '
+echo '---------------------------------------------'
+for ((counter=1;counter<=10;counter++))
+do
+echo $counter
+done
